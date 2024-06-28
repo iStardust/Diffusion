188aef3e7df05cad3076b0af440a1d82c71a3362
iStardust <121236556+iStardust@users.noreply.github.com>
2024-06-28 15:22:09 +0800

    Add files via upload

--- train.py
+++ train.py
@@ -1,65 +1,47 @@
 import torch
-import numpy
 import trimesh
 from tqdm import tqdm
 import numpy as np
-from importlib.machinery import SourceFileLoader
 import os
-epoches = 15
-batchsize = 8
-dataloadpath = 'dataloader/dataload.py'
-dataloadname = 'dataloader'
-diffusionpath = 'diffusion/gaussian_diffusion.py'
-diffusionname = 'diffusion'
-modelpath = 'model/transformer.py'
-modelname = 'model'
-
-dataload = SourceFileLoader(dataloadname, dataloadpath).load_module()
-diffusionload = SourceFileLoader(diffusionname, diffusionpath).load_module()
-modelload = SourceFileLoader(modelname, modelpath).load_module()
-PATH = '../../dataset/airplane'
-
-
-def train(device, dtype, dataloader):
-    model = modelload.PointDiffusionTransformer(
-        device=device, dtype=dtype).to(device)
-    optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
-    scheduler = torch.optim.lr_scheduler.StepLR(
-        optimizer, step_size=10, gamma=0.5)
-    for epoch in tqdm(range(epoches)):
-        model.train()
-        pbar = tqdm(total=len(dataloader))
-        losses = 0
-        for pc in dataloader:
-            optimizer.zero_grad()
-            pc = pc.to(device)
-            t = np.random.rand(pc.shape[0]).astype(np.float32 if device==torch.device('mps') else np.float64)
-            
-            timestep = torch.randint(1,10,(pc.shape[0],)).to(device)
-            # predict = model(pc, timestep).to(device)
-            
-            diffusion = diffusionload.GaussianDiffusion(t, model).to(device)
-            diffusionloss = diffusion.forward(pc, timestep)
-            losses+=diffusionloss
-            diffusionloss.backward()
-            optimizer.step()
-            pbar.update(1)
-        scheduler.step()
-        pbar.close()
-        print(f"Epoch: {epoch}, Loss: {losses / len(dataloader)}")
-    torch.save({
-            'model_state_dict': model.state_dict(),
-            'optimizer_state_dict': optimizer.state_dict(),
-            'scheduler_state_dict': scheduler.state_dict()
-        }, os.path.join('diffusion_model.pth'))
+from diffusion.gaussian_diffusion import GaussianDiffusion
+from model.transformer import PointDiffusionTransformer
+from dataloader.dataload import Prepare_Dataset
 
 if __name__ == "__main__":
-    dataset, dataloader = dataload.Prepare_Dataset(PATH, batch_size=batchsize)
-    device = torch.device('cpu')
-    dtype = torch.float64
-    if torch.cuda.is_available():
-        device = torch.device('cuda')
-    if torch.backends.mps.is_available():
-        dtype = torch.float32
-        device = torch.device('mps')
-    train(device, dtype, dataloader)
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    batch_size = 32
+    lr = 1e-4
+    epochs = 1000
+    num_timesteps = 1000
+    file_path = "dataloader/dataset/dataset/airplane"
+    sample_points = 512
+    dataset, dataloader = Prepare_Dataset(
+        filepath=file_path,
+        batch_size=batch_size,
+        num_workers=4,
+        num_points=sample_points,
+    )
+    betas = np.linspace(0.0001, 0.02, num_timesteps, dtype=np.float64)
+
+    noise_predictor = PointDiffusionTransformer(
+        device=device, dtype=torch.float32, n_ctx=sample_points
+    ).to(device)
+    diffusion = GaussianDiffusion(betas, noise_predictor).to(device)
+    optimizer = torch.optim.Adam(noise_predictor.parameters(), lr=lr)
+    scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=epochs)
+
+    for epoch in range(epochs):
+        print("Epoch:{}/{} ".format(epoch + 1, epochs))
+        running_loss = []
+        for pc in tqdm(dataloader):
+            pc = pc.to(device)
+            pc = pc.permute(0, 2, 1)
+            optimizer.zero_grad()
+            batch_t = torch.randint(0, num_timesteps, (pc.shape[0],)).to(device)
+            loss = diffusion(pc, batch_t)
+            loss.backward()
+            optimizer.step()
+            running_loss.append(loss.item())
+        scheduler.step()
+        print("Loss: ", np.mean(running_loss))
