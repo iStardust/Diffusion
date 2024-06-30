dc73facb8955713da69997f7d56d6b82f1e4c52b
yixiaoyu <yixiaoyu@stu.pku.edu.cn>
2024-06-30 14:15:39 +0800

    Add config.py, Adjust hyperparameters

---
+++ config.py
@@ -0,0 +1,10 @@
+CONFIG = {
+    "num_timesteps": 1000,
+    "lr": 3e-4,
+    "bs": 10,
+    "epochs": 50,
+    "category": "airplane",
+    "num_workers": 4,
+    "beta_min": 0.0001,
+    "beta_max": 0.02,
+}
